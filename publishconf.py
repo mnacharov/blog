693630de37a2d51262e93e6646e65506dfbc7c6f
Mikhail Nacharov <mnach@ya.ru>
2018-10-07 16:04:02 +0500

    add webnach.ru to gitlab pages

--- publishconf.py
+++ publishconf.py
@@ -10,7 +10,7 @@ sys.path.append(os.curdir)
 from pelicanconf import *  # NOQA
 
 OUTPUT_PATH = 'public'
-SITEURL = 'https://mnach.gitlab.io/webnach'
+SITEURL = 'http://webnach.ru'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
